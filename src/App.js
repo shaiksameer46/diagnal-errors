a93e42fb06ed4bc60907ae9040cecc72c47d06dc
shaiksameer46 <shaiksameer46@outlook.com>
2019-04-30 02:39:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
 import React, { Component } from 'react';
 import axios from "axios";
-import poster1 from './Slices/poster1.jpg';
 import './App.css';
+
 class App extends Component {
-
   state = {
-    data : {},
-    text : "./Slices/poster2.jpg"
-
+    data:{}
   };
 
   componentDidMount() {
-    axios.get(`./API/CONTENTLISTINGPAGE-PAGE1.json`)
+    axios.get(`/API/CONTENTLISTINGPAGE-PAGE1.json`)
       .then(res => {
-        const data = res.data;
-        this.setState({ data });
-        this.setState({ text : this.state.data.page.contents.content[0].image})
-        console.log(this.state.text);
+         const data = res.data;
+        this.setState({ data : data.content });
+        console.log(this.state.data[0].image);
       })
   }
 
   render() {
-    
+    console.log(this.state.data[0].image);
     return (
-      <img src={poster1} alt="poster1.jpg"/>
+      <img src = "/Slices/Poster1.jpg" alt = "poster1" />
     );
   }
 }
