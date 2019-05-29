fdd35dd1eb57baaf91d75b23f552860c174d9502
shaiksameer46 <47201332+shaiksameer46@users.noreply.github.com>
2019-05-29 11:39:44 +0530

    Update Info.jsx

--- src/Info.jsx
+++ src/Info.jsx
@@ -1,42 +1,73 @@
 import React, { Component } from 'react';
 import axios from "axios";
+import Loggedin from "./Loggedin";
+import { Route, Switch, Link} from 'react-router-dom';
 import './App.css';
 
 class Info extends Component {
     state = { 
-      number:this.props.match.params.id,
       data:[]
      }
- 
+
+
+     loadingdata = (id) => {
+      axios.get(`/API/CONTENTLISTINGPAGE-PAGE${id}.json`)
+        .then(res => {
+           const data = res.data;
+          this.setState({ data : data.content });
+          console.log(id);
+        })          
+    }
 
 
      componentDidMount() {
-        axios.get(`/API/CONTENTLISTINGPAGE-PAGE${this.state.number}.json`)
-          .then(res => {
-             const data = res.data;
-            this.setState({ data : data.content });
-            console.log(this.props.match.params.id);
-          })
-          
+            const id = this.props.match.params.id;
+            this.loadingdata(id); 
+      }
+       
+
+      componentWillReceiveProps(nextProps) {
+        if (nextProps.match.params.id !== this.props.match.params.id) {
+          const id = nextProps.match.params.id
+          this.loadingdata( id );
+        }
       }
 
     render() { 
-        if(!this.props.match.params.id){
-           this.setState({ number : "1" });
-        }
         return ( 
+          <React.Fragment>
+            <Loggedin />
             <div className="row">
-          {this.state.data.length ? (
-            this.state.data.map(pic => (
-              <div className="txtstyle">
-                <img src={pic.image} alt={pic.image} />
-                <p>{pic.name}</p>
+              {this.state.data.length ? (
+                this.state.data.map(pic => (
+                  <div className="txtstyle">
+                    <img src={pic.image} alt={pic.image} />
+                    <p>{pic.name}</p>
+                  </div>
+                ))
+              ) : (
+                  <img src="#" alt="not found" />
+                )}
+            </div>
+
+            <div className="row">
+              <div className="col xs-6-push layout">
+                <ul className="pagination">
+                  <li>
+                    <Link className="active" to="1">
+                      1
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="2">2</Link>
+                  </li>
+                  <li>
+                    <Link to="3">3</Link>
+                  </li>
+                </ul>
               </div>
-            ))
-          ) : (
-            <img src="#" alt="not found" />
-          )}
-        </div>
+            </div>
+          </React.Fragment>
          );
     }
 }
