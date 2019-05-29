296dea24db1e4e6be69200b4db2189f43231ea57
shaiksameer46 <47201332+shaiksameer46@users.noreply.github.com>
2019-05-29 11:38:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,38 @@
 import React, { Component } from 'react';
 import Info from "./Info";
-import { Route, Link, Switch } from 'react-router-dom';
+import First from "./First";
+import { Route, Switch, Link} from 'react-router-dom';
+import Login from "./Login";
 import './App.css';
+import Navbar from "./Navbar";
 
 class App extends Component {
 
   render() {
+    //console.log(this.props.match.params.id);
     return (
       <React.Fragment>
         <div id="overlay-container">
-        <Switch>
-          <Route
-            path="/:id"
-            render={props => <Info {...props} />}
-          />
-        </Switch>
-        </div>
-        
-        <div className="row">
-          <div className="col xs-6-push layout">
-            <ul class="pagination">
-              <li>
-                <Link className="active" to="1">
-                  1
-                </Link>
-              </li>
-              <li>
-                <Link to="2">2</Link>
-              </li>
-              <li>
-                <Link to="3">3</Link>
-              </li>
-            </ul>
-          </div>
+          <Switch>
+            <Route
+              path="/Login"
+              component={Login}
+            />
+            <Route path="/:id"
+              render={props => <Info {...props} />
+              }
+            />
+            <Route
+              path="/"
+              component={Login}
+            />
+          </Switch>
         </div>
       </React.Fragment>
     );
   }
+  
 }
 
 export default App;
+
